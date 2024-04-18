ed357bf18955775de2999084cdb3f25ecb2dfd2b
narumincho <16481886+narumincho@users.noreply.github.com>
2024-04-18 11:37:50 +0900

    add domParserDenoLand.ts

---
+++ example/domParserDenoLand.ts
@@ -0,0 +1,9 @@
+import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.45/deno-dom-wasm.ts";
+
+onmessage = async () => {
+  const dom = new DOMParser().parseFromString(
+    await (await fetch("https://www.npmjs.com/package/dom-parser")).text(),
+    "text/html"
+  );
+  postMessage(dom?.getElementsByTagName("h2").map((e) => e.textContent));
+};
