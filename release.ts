98fb999033b58363bb98631243588102b30aad37
narumincho <16481886+narumincho@users.noreply.github.com>
2024-04-25 11:21:21 +0900

    graphql api

--- release.ts
+++ release.ts
@@ -1,12 +1,101 @@
-import { exec, getExecOutput } from "npm:@actions/exec";
-import { getOctokit } from "npm:@actions/github";
+import { exec } from "npm:@actions/exec";
 
-console.log("env", Deno.env.toObject());
+function getTagNameOrRef(): string {
+  const githubRef = Deno.env.get("GITHUB_REF");
+  if (githubRef === undefined) {
+    throw new Error("GITHUB_REF is not set");
+  }
+  return /^refs\/tags\/(.+)$/u.exec(githubRef)?.[1] ?? githubRef;
+}
 
-const helpText = await getExecOutput(Deno.execPath(), ["compile", "--help"]);
+async function getOrCreateReleaseAndRelease(props: {
+  tagNameOrRef: string;
+  repositoryOwner: string;
+  repositoryName: string;
+}): Promise<{ releaseId: string; assets?: ReadonlyMap<string, string> }> {
+  const response = await octokit.graphql<{
+    repository: null | {
+      id: string;
+      releaseAssets: {
+        nodes: ReadonlyArray<{
+          id: string;
+          name: string;
+        }>;
+      };
+    };
+  }>(
+    `
+query ($owner: String!, $name: String!) {
+  repository(owner: $owner, name: $name) {
+    release(tagName: "") {
+      id
+      releaseAssets(first: 100) {
+        nodes {
+          id
+          name
+        }
+      }
+    }
+  }
+}
+`,
+    {
+      owner: repositoryOwner,
+      repo: repositoryName,
+    },
+  );
+  if (response.repository !== null) {
+    return {
+      releaseId: response.repository.id,
+      assets: new Map(
+        response.repository.releaseAssets.nodes.map((asset) => [
+          asset.name,
+          asset.id,
+        ]),
+      ),
+    };
+  }
+  console.log(`not found release by tag (${tagNameOrRef}) creating...`);
+  const release = await octokit.rest.repos.createRelease({
+    owner: props.repositoryOwner,
+    repo: props.repositoryName,
+    tag_name: tagNameOrRef,
+    make_latest: "true",
+    generate_release_notes: true,
+  });
+  return { releaseId: release.data.id };
+}
+
+async function uploadOrUpdateReleaseAsset(props: {
+  releaseId: number;
+  repositoryOwner: string;
+  repositoryName: string;
+  name: string;
+}): Promise<void> {
+  await octokit.rest.repos.getReleaseAsset({
+    owner: props.repositoryOwner,
+    repo: props.repositoryName,
+    release_id: props.releaseId,
+    name: "sample",
+  });
+  await octokit.rest.repos.uploadReleaseAsset({
+    owner: props.repositoryOwner,
+    repo: props.repositoryName,
+    release_id: props.releaseId,
+    name: "sample",
+    data: "only text?",
+  });
+}
+
+const tagNameOrRef = getTagNameOrRef();
+console.log("tagNameOrRef", tagNameOrRef);
+
+const helpText = await new Deno.Command(Deno.execPath(), {
+  args: ["compile", "--help"],
+}).output();
 
 const valuesText = /--target <target>[\w\W]+?\[possible values:([^\]]+)/gu.exec(
-  helpText.stdout
+  new TextDecoder().decode(helpText.stdout),
 )?.[1];
 
 if (valuesText === undefined) {
@@ -15,7 +104,15 @@ if (valuesText === undefined) {
 const possibleValues = valuesText.split(",").map((value) => value.trim());
 
 for (const target of possibleValues) {
-  await exec(Deno.execPath(), ["compile", "--target", target]);
+  await exec(Deno.execPath(), [
+    "compile",
+    "--target",
+    target,
+    "./cli.ts",
+    "--output",
+    // windows の場合はファイル名末尾に exe が付く
+    target,
+  ]);
 }
 const githubToken = Deno.env.get("GITHUB_TOKEN");
 if (githubToken === undefined) {
@@ -31,8 +128,18 @@ if (repositoryName === undefined) {
   throw new Error("GITHUB_REPOSITORY_NAME is not set");
 }
 
-const response = await octokit.rest.repos.getReadme({
+const readme = await octokit.rest.repos.getReadme({
   owner: repositoryOwner,
   repo: repositoryName,
 });
-console.log(response);
+console.log("readme", readme);
+
+await uploadOrUpdateReleaseAsset({
+  repositoryOwner: repositoryOwner,
+  repositoryName: repositoryName,
+  releaseId: await getOrCreateRelease({
+    repositoryName,
+    repositoryOwner,
+    tagNameOrRef,
+  }),
+});
