d39f06fe6241d591d7fea69ccfb791c503e8e0dd
narumincho <16481886+narumincho@users.noreply.github.com>
2024-12-10 11:48:14 +0900

    add AdaptorMember type in type.ts

--- type.ts
+++ type.ts
@@ -46,3 +46,55 @@ export type AdaptorAccount = {
    */
   tagNames?: string[];
 };
+
+export type AdaptorMember = {
+  /**
+   * 識別子
+   */
+  identifier?: string;
+
+  /**
+   * 社員番号
+   */
+  code?: string;
+
+  /**
+   * 名前
+   */
+  name?: string;
+
+  /**
+   * 部署
+   */
+  department?: string;
+
+  /**
+   * 役職
+   */
+  position?: string;
+
+  /**
+   * メールアドレス
+   */
+  email?: string;
+
+  /**
+   * サブメールアドレス一覧
+   */
+  subemails?: string[];
+
+  /**
+   * 画像
+   */
+  image?: string;
+
+  /**
+   * タグ名一覧
+   */
+  tagNames?: string[];
+
+  /**
+   * カスタムフィールド一覧
+   */
+  values?: Record<string, string>;
+};
